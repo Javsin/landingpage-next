4ad67779941371478841d025fb3995da453166a7
javsin <anaswidayanto@gmail.com>
2022-11-18 13:42:45 +0700

    push

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import NextNProgress from "nextjs-progressbar";
+import {QueryClient, QueryClientProvider} from 'react-query'
 function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient()
   return (
     <>
       <NextNProgress color="#F37020" height={3} startPosition={0.3} stopDelayMs={150}/>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   ) 
 }
