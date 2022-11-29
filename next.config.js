8e67942eb0de2a1afd44e6d473d55d097390b831
javsin <anaswidayanto@gmail.com>
2022-11-29 15:54:54 +0700

    push

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {
-    api : "https://landingpage-next-eight.vercel.app/api/",
+    api : "./",
     api_external : "https://external.murapay.id:6585/api/external/",
   }
 }
