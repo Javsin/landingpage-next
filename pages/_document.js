b6a5d1cd602d86fab9aad3a45d13b19051b90b4a
afifputra <afifputraharyono@gmail.com>
2023-02-07 09:57:36 +0700

    fix scrolling

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <link href="img/favicon.png" rel="icon" />
         <link href="img/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="vendor/aos/aos.css" rel="stylesheet" />
-        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        {/* <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" /> */}
         <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
         <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
         {/* <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet"/> */}
