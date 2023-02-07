1f56fbab36f62c28f2c910d8ca1d56097b1e750c
afifputra <afifputraharyono@gmail.com>
2023-02-07 14:59:52 +0700

    update title

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ const About = () => {
     });
   }, []);
   return (
-    <Layout title="Harga | Jualan Murah, Untung, Pelayanan Ramah, dan Transaksi Aman" description="Jualan Pulsa Murah Dengan Murapay" keyword="murapay,harga produk,pulsa murah,server pulsa">
+    <Layout title="Tentang Kami | Jualan Murah, Untung, Pelayanan Ramah, dan Transaksi Aman" description="Jualan Pulsa Murah Dengan Murapay" keyword="murapay,harga produk,pulsa murah,server pulsa">
       <AboutComponent />
     </Layout>
   );
