cc79c44a8e974815f7b57a9d50713aec8217342b
javsin <anaswidayanto@gmail.com>
2023-05-27 08:50:31 +0700

    modal_open

--- components/addons/modalStatic.js
+++ components/addons/modalStatic.js
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import { useState,useEffect } from 'react';
+import { useState , useEffect } from 'react';
 const customStyles = {
     content: {
         top: '50%',
@@ -17,7 +17,7 @@ const customStyles = {
         textAlign: "justify",
     },
 };
-const modalStatic = () => {
+const ModalStatic = () => {
     const [modalIsOpen, setIsOpen] = useState(false);
 
     const openModal = () => {
@@ -89,7 +89,7 @@ const modalStatic = () => {
                         <b>Jangan transfer dana yang diminta oknum</b> tersebut untuk mencegah terjadinya kerugian finansial.
                     </li>
                     <li className='fs-6_5'>
-                        Segera hubungi tim CS kami melalui Whatsapp di nomor 0813-2970-1020 (atau klik <a target='_blank' href='https://api.whatsapp.com/send/?phone=6281329701020&text=Hallo,ada%20oknum yang%20menghubungi%20dan meminta saya%20 untuk mentransfer%20 dana ke rekening %20murapay%20&type=phone_number&app_absent=0'>di sini</a>). Silahkan sampaikan kronologis yang lengkap agar tim kami bisa segera memblokir akun oknum tersebut.
+                        Segera hubungi tim CS kami melalui Whatsapp di nomor 0813-2970-1020 (atau klik <a target='_blank' rel="noreferrer" href='https://api.whatsapp.com/send/?phone=6281329701020&text=Hallo,ada%20oknum yang%20menghubungi%20dan meminta saya%20 untuk mentransfer%20 dana ke rekening %20murapay%20&type=phone_number&app_absent=0'>di sini</a>). Silahkan sampaikan kronologis yang lengkap agar tim kami bisa segera memblokir akun oknum tersebut.
                     </li>
                 </ol>
                 <button className='btn btn-sm btn-danger fw-bold' onClick={closeModal}>YA, SAYA MENGERTI</button>
@@ -98,4 +98,4 @@ const modalStatic = () => {
     )
 };
 
-export default modalStatic;
+export default ModalStatic;
